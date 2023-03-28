d0f15cf51629db328c53c3091bbe34b7e0062269
Fahim Muntasir Ruby <123890792+Fahim-99@users.noreply.github.com>
2023-03-29 00:32:04 +0600

    Delete Lab Task 6 directory

--- Lab Task 6/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    setcookie('status', '', time()-3600, '/');
-    setcookie('userid', '', time()-3600, '/');
-    header("location: login.php")
-?>
--- Lab Task 6/payment.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once 'header.php';
-    echo "Pay here<br>";
-    echo "Trip id is: ".$_SESSION['trip_id'];
-?>
