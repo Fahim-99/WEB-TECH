adcd8e1142f07e7fb462aaa8ef9d801aa98924ff
Fahim Muntasir Ruby <123890792+Fahim-99@users.noreply.github.com>
2024-07-11 12:12:50 +0600

    Delete Project directory

--- Project/Final Term/view/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    setcookie('userid', '', time()-3600, '/');
-    setcookie('usertype', '', time()-3600, '/');
-    setcookie('userimage', '', time()-3600, '/');
-    header("location: login.php")
-?>
--- Project/Final Term/view/user_header.php
+++
@@ -1,6 +0,0 @@
-<h2>
-    <a href="profile.php" id="profile">See profile</a>|
-    <a href="trip_history.php" id="trip-history">Trip History</a>|
-    <a href="change_profile_picture.php" id="change-profile">Change Profile</a>|
-    <a href="change_password.php" id="change-pass">Change Password</a><br><br>
-</h2>
